a0113631cedaa180a592a41db31e5d432f2a6e44
XionWin <xionwin@hotmail.com>
2021-02-06 02:25:48 +0800

    Update

--- Graphic/Drawing/Color/Extension.cs
+++ Graphic/Drawing/Color/Extension.cs
@@ -8,18 +8,18 @@ namespace Graphic.Drawing.Color
         {
             var hsl = new HSLA();
             // Convert RGB to a 0.0 to 1.0 range.
-            double double_r = rgb.R / 255.0;
-            double double_g = rgb.G / 255.0;
-            double double_b = rgb.B / 255.0;
+            double r = rgb.R / 255.0;
+            double g = rgb.G / 255.0;
+            double b = rgb.B / 255.0;
 
             // Get the maximum and minimum RGB components.
-            double max = double_r;
-            if (max < double_g) max = double_g;
-            if (max < double_b) max = double_b;
+            double max = r;
+            if (max < g) max = g;
+            if (max < b) max = b;
 
-            double min = double_r;
-            if (min > double_g) min = double_g;
-            if (min > double_b) min = double_b;
+            double min = r;
+            if (min > g) min = g;
+            if (min > b) min = b;
 
             double diff = max - min;
             hsl.L = (max + min) / 2;
@@ -33,13 +33,13 @@ namespace Graphic.Drawing.Color
                 if (hsl.L <= 0.5) hsl.S = diff / (max + min);
                 else hsl.S = diff / (2 - max - min);
 
-                double r_dist = (max - double_r) / diff;
-                double g_dist = (max - double_g) / diff;
-                double b_dist = (max - double_b) / diff;
+                double rd = (max - r) / diff;
+                double gd = (max - g) / diff;
+                double bd = (max - b) / diff;
 
-                if (double_r == max) hsl.H = b_dist - g_dist;
-                else if (double_g == max) hsl.H = 2 + r_dist - b_dist;
-                else hsl.H = 4 + g_dist - r_dist;
+                if (r == max) hsl.H = bd - gd;
+                else if (g == max) hsl.H = 2 + rd - bd;
+                else hsl.H = 4 + gd - rd;
 
                 hsl.H *= 60;
                 if (hsl.H < 0) hsl.H += 360;
